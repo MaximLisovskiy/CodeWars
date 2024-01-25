e0231a5fd82035c870a38e5b28a68df1e595f26d
Maxim Lisovskiy <chinatri@gmail.com>
2024-01-25 13:00:32 +0200

    add print

--- Tip_Calculator.py
+++ Tip_Calculator.py
@@ -1,15 +1,20 @@
 import math
+import time
+
+
 def calculate_tip(amount, rating):
     if rating.lower() == "terrible":
         return 0
     if rating.lower() == "poor":
-        return math.ceil((amount*5)/100)
+        return math.ceil((amount * 5) / 100)
     if rating.lower() == "good":
-        return math.ceil((amount*10)/100)
+        return math.ceil((amount * 10) / 100)
     if rating.lower() == "great":
         return math.ceil((amount * 15) / 100)
     if rating.lower() == "excellent":
         return math.ceil((amount * 20) / 100)
     else:
         return "Rating not recognised"
-print(calculate_tip(107.65, "GReat")) #-> 2
+    print("hello worls")
+
+print(calculate_tip(107.65, "GReat"))  # -> 2
